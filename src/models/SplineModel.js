d1e7a27e6443ccea078f1b77cf5fa6a8cc1faca2
Devesh <deveshdaku@gmail.com>
2024-05-24 20:08:50 +0530

    implemented spline 3d

---
+++ src/models/SplineModel.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+const SplienModel = () =>{
+    return (
+        <div className='spline'>
+            <Spline scene="https://prod.spline.design/c-vxRDomz4P1nuRi/scene.splinecode" />
+        </div>
+    );
+}
+
+export default SplienModel ;
